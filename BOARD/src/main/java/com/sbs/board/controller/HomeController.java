31d90e71318218c33f38b39ed0d0bc3e1dc94100
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-01-07 14:35:45 +0900

    메인화면TEST
    의존성주입
    DB생성 및 연결
    메인화면TEST

---
+++ BOARD/src/main/java/com/sbs/board/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.sbs.board.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HomeController {
+	@RequestMapping("/home/main")
+	@ResponseBody
+	public String showMain() {
+		return "메인화면 입니다.";
+	}
+	
+
+}
