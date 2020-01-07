90681769abbccb9b547c7448ec89f638ff1101d4
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-01-07 15:34:30 +0900

    article/list로 접속하면 list.jsp 이동

---
+++ BOARD/src/main/java/com/sbs/board/controller/ArticleController.java
@@ -0,0 +1,17 @@
+package com.sbs.board.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class ArticleController {
+	@RequestMapping("/article/list")
+	public String showMain() {
+		return "article/list";
+	}
+	
+
+}
