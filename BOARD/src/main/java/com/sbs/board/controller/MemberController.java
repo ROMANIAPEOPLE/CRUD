107f2aec0397b8f4e91b765445ef520eab766ec2
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-01-18 23:15:42 +0900

    showMain 수정
    ArticleController 의 showMain에서 직접 세션에 접근하는 코드 제거
    (인터셉트로 대체)

--- BOARD/src/main/java/com/sbs/board/controller/MemberController.java
+++ BOARD/src/main/java/com/sbs/board/controller/MemberController.java
@@ -29,20 +29,22 @@ public class MemberController {
 	@RequestMapping("/member/doLogout")
 	public String doLogout(HttpSession session) {
 		session.removeAttribute("loginedMemberId");
+		//로그아웃하면 세션에 저장된 loginId정보를 세션에서 제거한다.
 		return "redirect:/";
-		
 	}
-	
 	
 	@RequestMapping("/member/doLogin")
 	public String doLogin(@RequestParam Map<String, Object> param, Model model, HttpSession session) {
 		Members matchedMember = memberService.getMatchedOne((String) param.get("loginId"),
 				(String) param.get("loginPw"));
+		//matchedMember 변수에  loginId와 loginPw 두 개의 정보가 일치하는 정보가 있는지 확인한다. 일치한다면 null이 아닌 값이 저장될것이다.
 
 		if (matchedMember == null) {
 			model.addAttribute("alertMsg", "일치하는 회원이 존재하지 않습니다.");
 			model.addAttribute("historyBack", true);
 			return "common/redirect";
+			//DB에 저장되어있지 않은 id와pw정보가 입력되었다면 , 위와같은 얼럿창을 리턴한다.
+			
 		}
 
 		session.setAttribute("loginedMemberId", matchedMember.getId());
@@ -60,15 +62,27 @@ public class MemberController {
 	
 	@RequestMapping("/member/doJoin")
 	public String doJoin(@RequestParam Map<String, Object> param, Model model) {
-		//로그인 ID의 중복성 체크
-		Map<String, Object> checkLoginIdDupRs=memberService.checkLoginIdDup((String)param.get("loginId"));//ID 중복체크
+		// 로그인 ID의 중복성 체크
+		Map<String, Object> checkLoginIdDupRs = memberService.checkLoginIdDup((String) param.get("loginId"));
+
 		if (((String) checkLoginIdDupRs.get("resultCode")).startsWith("F-")) {
 			model.addAttribute("alertMsg", checkLoginIdDupRs.get("msg"));
 			model.addAttribute("historyBack", true);
 			return "common/redirect";
 		}
 
-		return "";
+		Map<String, Object> joinRs = memberService.join(param);
+
+		if (((String) joinRs.get("resultCode")).startsWith("F-")) {
+			model.addAttribute("alertMsg", joinRs.get("msg"));
+			model.addAttribute("historyBack", true);
+			return "common/redirect";
+		}
+
+		model.addAttribute("alertMsg", joinRs.get("msg"));
+		model.addAttribute("redirectUrl", "/member/login");
+
+		return "common/redirect";
 	}
 	
 }
