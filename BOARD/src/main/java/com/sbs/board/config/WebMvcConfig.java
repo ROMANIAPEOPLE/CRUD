537a65d6b522e87583f96ccf89092afda47a6238
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-01-19 14:11:15 +0900

    인터셉터 도입
    인터셉터를 도입하여 로그인상태와 비로그인상태에서 접근할 수 있는 페이지 설정

--- BOARD/src/main/java/com/sbs/board/config/WebMvcConfig.java
+++ BOARD/src/main/java/com/sbs/board/config/WebMvcConfig.java
@@ -9,16 +9,36 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-	//beforeActionInterceptor 불러오는 작업
+	// beforeActionInterceptor 불러오는 작업
 	@Autowired
 	@Qualifier("beforeActionInterceptor")
 	HandlerInterceptor beforeActionInterceptor;
-	
+
+	// needToLoginInterceptor 불러오는 작업
+	@Autowired
+	@Qualifier("needToLoginInterceptor")
+	HandlerInterceptor needToLoginInterceptor;
+
+	// needToLogoutInterceptor 불러오는 작업
+	@Autowired
+	@Qualifier("needToLogoutInterceptor")
+	HandlerInterceptor needToLogoutInterceptor;
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		//beforeActionInterceptor를 모든 액션(/**)에 연결한다.
-		//단 , resource로 시작하는 액션은 제외.
-		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**");
-	}
+		// beforeActionInterceptor를 모든 액션(/**)에 연결한다.
+		// 단 , resource로 시작하는 액션은 제외.
+		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**");	
+		
+		//   (/**) 는 로그인 된 상태로만 접근이 가능한데,   (/resource/**) 와   member/login , member/doLogin/ , member/join , member/doJoin
+		//  Home 은 비로그인으로도 접근이 가능하다 (예외)
+		registry.addInterceptor(needToLoginInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
+				.excludePathPatterns("/").excludePathPatterns("/member/login").excludePathPatterns("/member/doLogin")
+				.excludePathPatterns("/member/join").excludePathPatterns("/member/doJoin");
 
+	
+		//로그아웃 상태에서는 member/login , doLogin ,  join , doJoin 만 접근 가능하다.
+		registry.addInterceptor(needToLogoutInterceptor).addPathPatterns("/member/login")
+						.addPathPatterns("/member/doLogin").addPathPatterns("/member/join").addPathPatterns("/member/doJoin");
+	}
 }
